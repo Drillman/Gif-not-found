b2181a483bdfe4a232cd87139fb7ab187b7da93d
Amin NAIRI <nairi.amin@gmail.com>
2019-06-21 12:12:51 +0200

    :sparkles: added vuejs

---
+++ plugins/gif_not_found/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.addEventListener('load', function() {
+    new Vue({
+        el: '#git_not_found',
+        render: h => h('h1', 'hello, gif!')
+    });
+});
--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -8,5 +8,9 @@
 * Author URI: https://github.com/Drillman/Gif-not-found
 **/
 
-global $wpdb;
-$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options WHERE option_id = 1", OBJECT );
+add_action('wp_enqueue_scripts','gif_not_found_scripts');
+
+function gif_not_found_scripts() {
+    wp_enqueue_script( 'gif_not_found_script', plugins_url( '/index.js', __FILE__ ));
+    wp_enqueue_script( 'gif_not_found_vuejs', plugins_url( '/vue.js', __FILE__ ));
+}
