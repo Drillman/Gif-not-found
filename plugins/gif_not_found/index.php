03149d37a952553389a950f7e9256b7e33a49a23
Amin NAIRI <nairi.amin@gmail.com>
2019-06-21 12:22:53 +0200

    :pencil2: phpcs fixes

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -7,18 +7,20 @@ Author: Bruuuuuh
 Version: 1.0
 Author URI: https://motoriders.fr/
 */
-function default_short_code(){
-  echo "<h1>Ton père la saucisse</h1>";
-  // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
+function default_short_code()
+{
+    echo "<h1>Ton père la saucisse</h1>";
+    // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
 }
 
 add_shortcode('gif_not_found', '<h1>Weeeeshshhshshshshsh</h1>');
 // require(plugin_dir_path(__FILE__).'shortcode.php');
 
 
-add_action('wp_enqueue_scripts','gif_not_found_scripts');
+add_action('wp_enqueue_scripts', 'gif_not_found_scripts');
 
-function gif_not_found_scripts() {
-    wp_enqueue_script( 'gif_not_found_script', plugins_url( '/index.js', __FILE__ ));
-    wp_enqueue_script( 'gif_not_found_vuejs', plugins_url( '/vue.js', __FILE__ ));
+function gif_not_found_scripts()
+{
+    wp_enqueue_script('gif_not_found_script', plugins_url('/index.js', __FILE__));
+    wp_enqueue_script('gif_not_found_vuejs', plugins_url('/vue.js', __FILE__));
 }
--- plugins/gif_not_found/shortcode.php
+++ plugins/gif_not_found/shortcode.php
@@ -1,7 +1,8 @@
 <?php
-function default_short_code(){
-  echo "<h1>Ton père la saucisse</h1>";
-  // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
+function default_short_code()
+{
+    echo "<h1>Ton père la saucisse</h1>";
+    // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
 }
 
 add_shortcode('gif_not_found', 'default_short_code');
