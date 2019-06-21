b0f4ff6d3f4d041ba28d633522a37e817ced747d
Lucas <xz351@hotmail.fr>
2019-06-21 10:55:52 +0200

    Create gif not found plugin

---
+++ plugins/gif_not_found/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Gif not found
+Plugin URI: https://motoriders.fr/
+Description: Display a gif on the 404 page
+Author: Bruuuuuh
+Version: 1.0
+Author URI: https://motoriders.fr/
+*/
