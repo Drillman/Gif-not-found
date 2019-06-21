1aa1519d7a789fa4eb4143c1d015261dea1afa24
Thomas Corio <Thomas.Corio@gmail.com>
2019-06-21 12:32:03 +0200

    fix bug

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -7,13 +7,8 @@ Author: Bruuuuuh
 Version: 1.0
 Author URI: https://motoriders.fr/
 */
-function default_short_code(){
-  echo "<h1>Ton père la saucisse</h1>";
-  // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
-}
 
-add_shortcode('gif_not_found', '<h1>Weeeeshshhshshshshsh</h1>');
-// require(plugin_dir_path(__FILE__).'shortcode.php');
+require(plugin_dir_path(__FILE__).'shortcode.php');
 
 
 global $wpdb;
