9614496e7bce1ad7e8b1b7ac7ce5394250bed00c
Amin NAIRI <nairi.amin@gmail.com>
2019-06-21 10:57:06 +0200

    :lipstick: phpcs fixes

--- plugins/database/index.php
+++ plugins/database/index.php
@@ -8,13 +8,14 @@ Version: 1.0
 Author URI: http://google.com/
 */
 
-function createTable(){
-  global $wpdb;
-  $table_name = 'Bruh';
+function createTable()
+{
+    global $wpdb;
+    $table_name = 'Bruh';
 
-  $charset_collate = $wpdb->get_charset_collate();
+    $charset_collate = $wpdb->get_charset_collate();
 
-  $sql = "CREATE TABLE $table_name (
+    $sql = "CREATE TABLE $table_name (
     id mediumint(9) NOT NULL AUTO_INCREMENT,
     time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
     name tinytext NOT NULL,
@@ -23,8 +24,8 @@ function createTable(){
     PRIMARY KEY  (id)
   ) $charset_collate;";
 
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  dbDelta( $sql );
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
 }
 
-register_activation_hook( __FILE__, 'createTable' );
+register_activation_hook(__FILE__, 'createTable');
