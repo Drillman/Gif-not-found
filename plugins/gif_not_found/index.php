0db83b98b6104ebd37f1a2b324ae792fc5f1bfc2
Amin NAIRI <nairi.amin@gmail.com>
2019-06-21 11:33:23 +0200

    :sparkles: fixed path to plugin

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -1,9 +1,12 @@
 <?php
-/*
-Plugin Name: Gif not found
-Plugin URI: https://motoriders.fr/
-Description: Display a gif on the 404 page
-Author: Bruuuuuh
-Version: 1.0
-Author URI: https://motoriders.fr/
-*/
+/**
+* Plugin Name: Gif not found
+* Plugin URI: https://github.com/Drillman/Gif-not-found
+* Description: Add gifs to your app!
+* Version: 1.0
+* Author: ESGI WordPress Team
+* Author URI: https://github.com/Drillman/Gif-not-found
+**/
+
+global $wpdb;
+$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options WHERE option_id = 1", OBJECT );
