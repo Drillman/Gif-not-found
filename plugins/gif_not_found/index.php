77e9589070d9718c7033047d1674ff946d8f45b3
Thomas Corio <Thomas.Corio@gmail.com>
2019-06-21 12:37:44 +0200

    shortcode fixed

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -7,14 +7,6 @@ Author: Bruuuuuh
 Version: 1.0
 Author URI: https://motoriders.fr/
 */
-<<<<<<< HEAD
-=======
-function default_short_code()
-{
-    echo "<h1>Ton père la saucisse</h1>";
-    // return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
-}
->>>>>>> 1cc2790dece166a290dabb25aa781d16bdc7b22c
 
 require(plugin_dir_path(__FILE__).'shortcode.php');
 
