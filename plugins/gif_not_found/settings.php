fe13a0693210d7e65eb83accc5270aa35019b70e
Thomas Corio <Thomas.Corio@gmail.com>
2019-07-01 15:45:29 +0200

    add setting pages

--- plugins/gif_not_found/settings.php
+++ plugins/gif_not_found/settings.php
@@ -6,55 +6,35 @@ function my_cool_plugin_create_menu() {
 
 	//create new top-level menu
   add_menu_page('Gif not found settings', 'Gif not found', 'administrator', 'gif_not_found_settings', 'elementsPage' );
-  add_submenu_page('gif_not_found_settings','Add a new gif', 'Add gif', 'administrator', 'gif_not_found_add', 'addGifPage');
+  // add_submenu_page('gif_not_found_settings','Add a new gif', 'Add gif', 'administrator', 'gif_not_found_add', 'addGifPage');
 
 	//call register settings function
-	add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+	// add_action( 'admin_init', 'register_my_cool_plugin_settings' );
 }
 
 
-function register_my_cool_plugin_settings() {
-  register_setting('gif_not_found-group', 'elements');
-  // register_setting('gif_not_found-group');
-  // register_setting('gif_not_found-group');
-  // register_setting('gif_not_found-group');
-}
+// function register_my_cool_plugin_settings() {
+//   register_setting('gif_not_found-group', 'elements');
+// }
 
 function elementsPage(){
-  $elements = get_option('elements')
   ?>
-  <div class="wrap">
-  <h1>Your GIFs</h1>
-  <?php do_settings_sections( 'gif_not_found-group' ); ?>
+  <h1>Documentation du plugin Gif not found</h1>
+  <p> pour utiliser le pluging il faut utiliser les shortcodes suivant : </p>
+  
   <table>
     <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Shortcode</th>
-      </tr>
+        <tr>
+            <th colspan="2">The table header</th>
+        </tr>
     </thead>
     <tbody>
-    <?php foreach($elements as $element): ?>
-      <tr>
-        <td><?php echo $element['id']; ?></td>
-        <td><?php echo $element['name']; ?></td>
-        <td><?php echo $element['shortcode']; ?></td>
-      </tr>
-    <?php endforeach ?>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+        </tr>
     </tbody>
-  </table>
-  </div>
+</table>
   <?php
 }
 
-function addElementPage(){ ?>
-  <div class="wrap">
-    <h1>Add a GIF</h1>
-    <form action="options.php" method="post">
-      <?php settings_fields('gif_not_found-group') ?>
-      <?php do_settings_sections( 'gif_not_found-group' ); ?>
-      <input type="text" name="">
-    </form>
-  </div>
-<?php }
