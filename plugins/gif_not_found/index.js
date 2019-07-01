ab4ce88908aa92804d1f1e2e13fc694ea9154fd8
Amin NAIRI <nairi.amin@gmail.com>
2019-07-01 15:44:10 +0200

    :sparkles: added shortcode parameter + slider amination

--- plugins/gif_not_found/index.js
+++ plugins/gif_not_found/index.js
@@ -1,10 +1,58 @@
 'use strict';
 
 const GifNotFoundSlider = {
+    data() {
+        return {
+            giphy: {
+                api: {
+                    endpoint: 'https://api.giphy.com/v1/gifs/random?api_key=Wk4vDI46cIETosiA8b71u8FoSqOSXqVN&tag='
+                },
+                image: {
+                    url: '',
+                    alt: '',
+                    style: {
+                        width: '30vw',
+                        height: '30vh'
+                    }
+                }
+            }
+        };
+    },
+
+    methods: {
+        loadGif() {
+            const tag = document.getElementById('gif_not_found_tag').dataset.tag || 'cat';
+
+            fetch(this.giphy.api.endpoint + tag).then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                }
+            }).then((data) => {
+                this.giphy.image.url = data.data.image_original_url;
+                console.log(data.data.image_original_url);
+            }).catch(function(error) {
+                console.error(error); 
+            });
+        }
+    },
+
+    mounted() {
+        const gifNotFoundParameters = document.getElementById('gif_not_found_tag').dataset;
+
+        this.loadGif();
+        this.giphy.image.style.width = gifNotFoundParameters.width || '30vw';
+        this.giphy.image.style.height = gifNotFoundParameters.height || '30vh';
+    },
+
     template: `
-        <h1>
-            Gif not found slider
-        </h1>
+        <transition name='slide-left' mode='out-in' appear>
+            <img
+                @click='loadGif'
+                :key='giphy.image.url'
+                :src='giphy.image.url'
+                :alt='giphy.image.alt'
+                :style='giphy.image.style' />
+        </transition>
     `
 };
 
