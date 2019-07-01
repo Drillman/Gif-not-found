3d2b640f31d1d849ca4f1125145c749212ed4c67
Thomas Corio <Thomas.Corio@gmail.com>
2019-07-01 22:09:49 +0200

    change name of plugin

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gif not found
 Plugin URI: https://motoriders.fr/
-Description: Display a gif on the 404 page
+Description: Display a gif
 Author: Bruuuuuh
 Version: 1.0
 Author URI: https://motoriders.fr/
