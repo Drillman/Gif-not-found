2a78f7048fc2aabc78079e25316813bbc8ce37ad
quentinhermiteau <quentinhermiteau@gmail.com>
2019-07-02 15:01:46 +0200

    fix indentation + documentation

--- plugins/gif_not_found/shortcode.php
+++ plugins/gif_not_found/shortcode.php
@@ -1,12 +1,8 @@
 <?php
-function default_short_code()
-{
-    return '<img src="https://www.kizoa.fr/img/e8nZC.gif" alt="Bruuuuuh">';
-}
 
-add_shortcode('gif_not_found', 'default_short_code');
 add_shortcode('gif_not_found_slider', 'gif_not_found_slider');
 
+// generate the gif slider
 function gif_not_found_slider($attributes)
 {
     $attributes = shortcode_atts([
