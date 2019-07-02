341f649f54d3c9b766e8b3c33aed47810e63acf7
quentinhermiteau <quentinhermiteau@gmail.com>
2019-07-02 14:48:07 +0200

    clean code + add documentation

--- plugins/gif_not_found/index.php
+++ plugins/gif_not_found/index.php
@@ -8,24 +8,26 @@ Version: 1.0
 Author URI: https://motoriders.fr/
 */
 
+// require all necessary files
 require(plugin_dir_path(__FILE__).'shortcode.php');
 require(plugin_dir_path(__FILE__).'settings.php');
 require(plugin_dir_path(__FILE__).'widget_gif_not_found.php');
 
+// declare functions to load required files
 add_action('wp_enqueue_scripts', 'gif_not_found_scripts');
 add_action('wp_enqueue_scripts', 'gif_not_found_stylesheets');
 
-
-
+// require all js files
 function gif_not_found_scripts()
 {
     wp_enqueue_script('gif_not_found_vuejs', plugins_url('/vue.js', __FILE__));
     wp_enqueue_script('gif_not_found_script', plugins_url('/index.js', __FILE__));
 }
 
+// require all css files
 function gif_not_found_stylesheets()
 {
-    wp_enqueue_style('gif_not_found_stylesheet', plugin_dir_url(__FILE__) . 'index.css', array(), null);
+    wp_enqueue_style('gif_not_found_stylesheet', plugin_dir_url(__FILE__) . 'index.css', [], null);
 }
 
 
--- plugins/gif_not_found/widget_gif_not_found.php
+++ plugins/gif_not_found/widget_gif_not_found.php
@@ -1,75 +1,75 @@
 <?php class Gif_not_found extends WP_Widget {
- 
- // Configuration globale du widget
- function __construct() {
+    // Configuration globale du widget
+    function __construct() {
+        $widget_args = [
+            'classname'     => 'Gif_not_found',
+            'description'   => 'Gif_not_found'
+        ];
 
-         $widget_args = array(
-                 'classname' => 'Gif_not_found',
-                 'description' => 'Gif_not_found'
-         );
+        $control_args = [
+            'width' => 350
+        ];
 
-         $control_args = array(
-                 'width' => 350
-         );
+        parent::__construct(
+            'gif_not_found_widget',
+            __('Image animée aléatoire'),
+            $widget_args,
+            $control_args
+        );
+    }
 
-         parent::__construct(
-                 'gif_not_found_widget',
-                 __('Image animée aléatoire'),
-                 $widget_args,
-                 $control_args
-         );
- }
-
- // Affichage en front-end
- function widget($args, $instance) {
+    // Affichage en front-end
+    function widget($args, $instance) {
         extract($args);
-        $tag = strip_tags($instance['tag']);
+        $tag    = strip_tags($instance['tag']);
         $height = strip_tags($instance['height']);
-        $width = strip_tags($instance['width']);
+        $width  = strip_tags($instance['width']);
         echo "<div id='gif_not_found_slider_widget'></div><input id='gif_not_found_slider_widget_data' type='hidden' data-tag='$tag' data-width='$width' data-height='$height' />";
-    
- }
 
- // Traitement des données avant sauvegarde
- function update( $new_instance, $old_instance ) {
-         $new_instance['tag'] = strip_tags($new_instance['tag']);
-         $new_instance['height'] = strip_tags($new_instance['height']);
-         $new_instance['width'] = strip_tags($new_instance['width']);
-         return $new_instance;
- }
+    }
 
- // Affichage du formulaire de réglages du widget en back-end
- function form($instance) {
-         $instance = wp_parse_args(
-                 $instance,
-                 array(
-                         'tag'    => 'cat',
-                         'height' => '30vh',
-                         'width'  => '30vw'  
-                 )
-         );
+    // Traitement des données avant sauvegarde
+    function update($new_instance, $old_instance) {
+        $new_instance['tag']    = strip_tags($new_instance['tag']);
+        $new_instance['height'] = strip_tags($new_instance['height']);
+        $new_instance['width']  = strip_tags($new_instance['width']);
+        return $new_instance;
+    }
 
-         $tag = strip_tags($instance['tag']);
-         $height = strip_tags($instance['height']);
-         $width = strip_tags($instance['width']);
-         ?>
+    // Affichage du formulaire de réglages du widget en back-end
+    function form($instance) {
+        $instance = wp_parse_args(
+            $instance,
+            [
+                'tag'    => 'cat',
+                'height' => '30vh',
+                'width'  => '30vw'  
+            ]
+        );
 
-         <p>
-                 <label for="<?php echo $this->get_field_id('tag'); ?>">Tag :</label>
-                 <input id="<?php echo $this->get_field_id('tag'); ?>" name="<?php echo $this->get_field_name('tag'); ?>" type="text" value="<?php echo esc_attr($tag); ?>" />
-         </p>
-         <p>
-                 <label for="<?php echo $this->get_field_id('height'); ?>">Hauteur :</label>
-                 <input id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo esc_attr($height); ?>" />
-         </p>
-         <p>
-                 <label for="<?php echo $this->get_field_id('width'); ?>">Largeur :</label>
-                 <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
-         </p>
- <?php }
+        $tag    = strip_tags($instance['tag']);
+        $height = strip_tags($instance['height']);
+        $width  = strip_tags($instance['width']);
+    ?>
+    <p>
+        <label for="<?php echo $this->get_field_id('tag'); ?>">Tag :</label>
+        <input id="<?php echo $this->get_field_id('tag'); ?>" name="<?php echo $this->get_field_name('tag'); ?>" type="text" value="<?php echo esc_attr($tag); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id('height'); ?>">Hauteur :</label>
+        <input id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo esc_attr($height); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id('width'); ?>">Largeur :</label>
+        <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
+    </p>
+<?php 
+    }
 }
 
+// Initialize the widget
 function init_gif_not_found_widget() {
- register_widget('Gif_not_found');
+    register_widget('Gif_not_found');
 }
+
 add_action('widgets_init', 'init_gif_not_found_widget');
